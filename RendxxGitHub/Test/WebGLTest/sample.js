76d3e5ace8af838693bfd5bee09c736e573b11ca
Rendxx <rendxx@gmail.com>
2016-04-20 19:17:37 -0500

    Upload webGL test

---
+++ RendxxGitHub/Test/WebGLTest/sample.js
@@ -0,0 +1,233 @@
+﻿//---------------------------------------------------------------------------------------------
+
+function render(images) {
+    var canvas = document.getElementById("canvas1");
+    var gl = canvas.getContext("webgl");
+
+    // setup a GLSL program
+    var program = createProgramFromScripts(gl, "2d-vertex-shader", "2d-fragment-shader");
+    gl.useProgram(program);
+    
+    // look up where the texture coordinates need to go.
+    var positionLocation = gl.getAttribLocation(program, "a_position");
+    var texCoordLocation = gl.getAttribLocation(program, "a_texCoord");
+
+    //-------------------------------------------------------------------------
+
+    // provide texture coordinates for the rectangle.
+    var texCoordBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+        0.0, 0.0,
+        1.0, 0.0,
+        0.0, 1.0,
+        0.0, 1.0,
+        1.0, 0.0,
+        1.0, 1.0]), gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(texCoordLocation);
+    gl.vertexAttribPointer(texCoordLocation, 2, gl.FLOAT, false, 0, 0);
+
+    //-------------------------------------------------------------------------
+    // create 2 textures
+    var textures = [];
+    for (var ii = 0; ii < 2; ++ii) {
+        var texture = gl.createTexture();
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+
+        // Set the parameters so we can render any size image.
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+        // Upload the image into the texture.
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, images[ii]);
+
+        // add the texture to the array of textures.
+        textures.push(texture);
+    }
+    var resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+
+    // lookup the sampler locations.
+    var u_image0Location = gl.getUniformLocation(program, "u_image0");
+    var u_image1Location = gl.getUniformLocation(program, "u_image1");
+
+    // set which texture units to render with.
+    gl.uniform1i(u_image0Location, 0);
+    gl.uniform1i(u_image1Location, 1);
+
+    // Set each texture unit to use a particular texture.
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, textures[0]);
+    gl.activeTexture(gl.TEXTURE1);
+    gl.bindTexture(gl.TEXTURE_2D, textures[1]);
+
+    // set the resolution
+    gl.uniform2f(resolutionLocation, canvas.width, canvas.height);
+
+    //-------------------------------------------------------------------------
+    
+    // Create a buffer for the position of the rectangle corners.
+    var buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.enableVertexAttribArray(positionLocation);
+    gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+
+    // Set a rectangle the same size as the image.
+    setRectangle(gl, 0, 0, images[0].width, images[0].height);
+
+    // Draw the rectangle.
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
+}
+
+function setRectangle(gl, x, y, width, height) {
+    var x1 = x;
+    var x2 = x + width;
+    var y1 = y;
+    var y2 = y + height;
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+       x1, y1,
+       x2, y1,
+       x1, y2,
+       x1, y2,
+       x2, y1,
+       x2, y2]), gl.STATIC_DRAW);
+}
+
+
+//---------------------------------------------------------------------------------------------
+function loadImage(url, callback) {
+    var image = new Image();
+    image.src = url;
+    image.onload = callback;
+    return image;
+}
+
+function loadImages(urls, callback) {
+    var images = [];
+    var imagesToLoad = urls.length;
+
+    var onImageLoad = function () {
+        --imagesToLoad;
+        if (imagesToLoad == 0) {
+            callback(images);
+        }
+    };
+
+    for (var ii = 0; ii < imagesToLoad; ++ii) {
+        var image = loadImage(urls[ii], onImageLoad);
+        images.push(image);
+    }
+}
+
+//---------------------------------------------------------------------------------------------
+/**
+ * Creates and compiles a shader.
+ *
+ * @param {!WebGLRenderingContext} gl The WebGL Context.
+ * @param {string} shaderSource The GLSL source code for the shader.
+ * @param {number} shaderType The type of shader, VERTEX_SHADER or
+ *     FRAGMENT_SHADER.
+ * @return {!WebGLShader} The shader.
+ */
+function compileShader(gl, shaderSource, shaderType) {
+    // Create the shader object
+    var shader = gl.createShader(shaderType);
+
+    // Set the shader source code.
+    gl.shaderSource(shader, shaderSource);
+
+    // Compile the shader
+    gl.compileShader(shader);
+
+    // Check if it compiled
+    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (!success) {
+        // Something went wrong during compilation; get the error
+        throw "could not compile shader:" + gl.getShaderInfoLog(shader);
+    }
+
+    return shader;
+}
+
+/**
+ * Creates a program from 2 shaders.
+ *
+ * @param {!WebGLRenderingContext) gl The WebGL context.
+ * @param {!WebGLShader} vertexShader A vertex shader.
+ * @param {!WebGLShader} fragmentShader A fragment shader.
+ * @return {!WebGLProgram} A program.
+ */
+function createProgram(gl, vertexShader, fragmentShader) {
+    // create a program.
+    var program = gl.createProgram();
+
+    // attach the shaders.
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+
+    // link the program.
+    gl.linkProgram(program);
+
+    // Check if it linked.
+    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (!success) {
+        // something went wrong with the link
+        throw ("program filed to link:" + gl.getProgramInfoLog(program));
+    }
+
+    return program;
+};
+
+
+/**
+ * Creates a shader from the content of a script tag.
+ *
+ * @param {!WebGLRenderingContext} gl The WebGL Context.
+ * @param {string} scriptId The id of the script tag.
+ * @param {string} opt_shaderType. The type of shader to create.
+ *     If not passed in will use the type attribute from the
+ *     script tag.
+ * @return {!WebGLShader} A shader.
+ */
+function createShaderFromScript(gl, scriptId, opt_shaderType) {
+    // look up the script tag by id.
+    var shaderScript = document.getElementById(scriptId);
+    if (!shaderScript) {
+        throw ("*** Error: unknown script element" + scriptId);
+    }
+
+    // extract the contents of the script tag.
+    var shaderSource = shaderScript.text;
+
+    // If we didn't pass in a type, use the 'type' from
+    // the script tag.
+    if (!opt_shaderType) {
+        if (shaderScript.type == "x-shader/x-vertex") {
+            opt_shaderType = gl.VERTEX_SHADER;
+        } else if (shaderScript.type == "x-shader/x-fragment") {
+            opt_shaderType = gl.FRAGMENT_SHADER;
+        } else if (!opt_shaderType) {
+            throw ("*** Error: shader type not set");
+        }
+    }
+
+    return compileShader(gl, shaderSource, opt_shaderType);
+};
+
+/**
+ * Creates a program from 2 script tags.
+ *
+ * @param {!WebGLRenderingContext} gl The WebGL Context.
+ * @param {string} vertexShaderId The id of the vertex shader script tag.
+ * @param {string} fragmentShaderId The id of the fragment shader script tag.
+ * @return {!WebGLProgram} A program
+ */
+function createProgramFromScripts(gl, vertexShaderId, fragmentShaderId) {
+    var vertexShader = createShaderFromScript(gl, vertexShaderId);
+    var fragmentShader = createShaderFromScript(gl, fragmentShaderId);
+    return createProgram(gl, vertexShader, fragmentShader);
+}
+
+
+
